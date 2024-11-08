22c9a212327bba8330d1c59688906b2c80e8c6bf
omgxlori <lbelovin@gmail.com>
2024-11-07 23:00:27 -0500

    Update SavedCandidates.tsx

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Candidate } from '../interfaces/Candidate.interface';
 
 const SavedCandidates = () => {
